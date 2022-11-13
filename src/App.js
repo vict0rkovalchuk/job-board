8fe7a81d1d4bacc89132e9b1c519381748dcb0b4
Victor <vict02r05@gmail.com>
2022-11-14 00:15:21 +0200

    add using scss instead of css and set up fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold text-center underline">Hello world!</h1>
+      <h1 className="text-3xl text-center underline">Hello world!</h1>
     </div>
   );
 }
